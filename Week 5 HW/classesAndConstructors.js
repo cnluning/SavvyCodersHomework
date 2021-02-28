34b38d52a7a5ba111cd5b9f05880aa595a1ab871
Claire Luning <cnluning@gmail.com>
2021-02-28 15:41:16 -0600

    upload completed week 5 homework

---
+++ Week 5 HW/classesAndConstructors.js
@@ -0,0 +1,22 @@
+class scaryMonster {
+    constructor (name, type, location) {
+        this.name = name;
+        this.type = type;
+        this.location = location;
+        this.bio = `${name} is a ${type} who lurks around ${location}!`;
+    }
+}
+
+class spookyGhost extends scaryMonster {
+    constructor (name, type, location, creepiness) {
+        super(name, type, location);
+        this.creepiness = creepiness;
+    }
+}
+
+class evilDemon extends spookyGhost {
+    constructor (name, type, location, wickedness) {
+        super(name, type, location);
+        this.wickedness = wickedness;
+    }
+}
